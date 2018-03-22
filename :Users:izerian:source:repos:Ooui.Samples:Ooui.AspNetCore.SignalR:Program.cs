57660e9a492ba3846231c6877034a3135a3887e0
solatticus <>
2018-03-22 08:14:42 -0400

    Updated entry point and appsettings

---
+++ :Users:izerian:source:repos:Ooui.Samples:Ooui.AspNetCore.SignalR:Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ooui.AspNetCore.SignalR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
+                .ConfigureLogging((context, factory) =>
+                {
+                    factory.AddConfiguration(context.Configuration.GetSection("Logging"));
+                    factory.AddConsole();
+                    factory.AddDebug();
+                })
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Ooui.Samples
 Simple samples using Ooui for webassemblies using .Net
-
-## March 2018
-Projects require the latest visual studio preview on Windows. 
-https://github.com/aspnet/Tooling/blob/master/dotnet-core-2.1.md
