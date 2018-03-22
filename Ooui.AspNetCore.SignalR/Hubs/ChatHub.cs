1e9588650705af4ca8286b4814f715965f673942
izerian <izerian@yahoo.com>
2018-03-22 07:42:59 -0400

    Initial Commit, adding things

---
+++ Ooui.AspNetCore.SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Ooui.AspNetCore.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            // send the message to all connected clients
+            await Clients.All.SendAsync("Send", Context.User.Identity.Name, message);
+        }
+    }
+}
---
+++ Ooui.WebAssembly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ooui.WebAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
