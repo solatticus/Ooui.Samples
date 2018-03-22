e7fa3d7cefd22118c040564e57b5892ec92ab0ff
solatticus <>
2018-03-22 10:12:08 -0400

    Web Assembly from AspNetCore using Xamarin works
    Skeleton should be good to go. Forms code is displaying properly, served from ASP.Net Core MVC.

--- Ooui.AspNetCore.SignalR/Controllers/HomeController.cs
+++ Ooui.AspNetCore.SignalR/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Xamarin.Forms; //needed for ContentPage.GetOouiElement() extension method
+using Ooui.Samples.Forms;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +17,13 @@ namespace Ooui.AspNetCore.SignalR.Controllers
         {
             ViewData["Title"] = "AspNetCore Ooui SignalR - Chat Sample";
             return View();
+        }
+
+        public ElementResult SignalRDemo()
+        {
+            var oe = new ChatSamplePage().GetOouiElement();
+            var er = new ElementResult(oe, "SignalR Sample");
+            return er;
         }
     }
 }
